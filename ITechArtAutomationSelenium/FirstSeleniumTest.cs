5634ca2dab012858f875161dce2f68d8044a44c2
Nodir <kebridon.nemat.life@gmail.com>
2022-12-27 18:25:16 +0500

    Add project files.

---
+++ ITechArtAutomationSelenium/FirstSeleniumTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ITechArtAutomationSelenium
+{
+    public class FirstSeleniumTest
+    {
+        [Test]
+        public void FirstTest()
+        {
+            var pathToChromeDriver = Path.Combine(AppContext.BaseDirectory, "Resources", "chromedriver.exe");
+            IWebDriver webDriver = new ChromeDriver(pathToChromeDriver);
+            var BaseUrl = "https://www.demoblaze.com/index.html";
+            var baseUrlobj = new Uri(BaseUrl, UriKind.Absolute);
+            webDriver.Navigate().GoToUrl(baseUrlobj);
+            webDriver.Quit(); //close the browser
+            // webDriver.Close(); close the tab in browser
+
+        }
+
+    }
+}
